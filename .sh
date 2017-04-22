72718334642f81f454e7024a00dc83f0b9b901fd
郭玉森 <ysguosx@163.com>
2017-04-22 15:08:19 +0900

    Delete .sh

--- .sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-lastday=$(date -d last-day +%Y%m%d)
-output="/home/kintaiapp_user/schedual/output/"${lastday}
-if [ ! -x $output ] ;then
-  mkdir $output
-fi
-java -jar /home/kintaiapp_user/schedual/AttendanceCSV.jar -o ${output}
